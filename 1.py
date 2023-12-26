5f9acd4e083776703a4fc6f030278b982b054316
beAgun <140337252+beAgun@users.noreply.github.com>
2023-12-26 23:02:51 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,7 +4,6 @@ from matplotlib.colors import ListedColormap
 
 
 def f2(i, j, n):
-    #arr = [[0 for _ in range(n)] for _ in range(n)]
     arr = np.add.outer(range(n), range(n)) % 2
 
     for col in range(1, n + 1):
@@ -18,7 +17,6 @@ def f2(i, j, n):
 
 
 def f1(i, j, n):
-    #arr = [[0 for _ in range(n)] for _ in range(n)]
     arr = np.add.outer(range(n), range(n)) % 2
 
     for row in range(i - 1, i + 1 + 1):
@@ -32,25 +30,17 @@ def f1(i, j, n):
 def draw_board(board, n):
 
     dx, dy = 0.015, 0.05
-    P = np.arange(0, 8, dx)
-    Q = np.arange(0, 8, dy)
+    P = np.arange(0, n, dx)
+    Q = np.arange(0, n, dy)
     P, Q = np.meshgrid(P, Q)
 
     min_max = np.min(P), np.max(P), np.min(Q), np.max(Q)
-    #min_max = -4, 4, -4, 4
-    #
-    # res = np.add.outer(range(8), range(8)) % 2
-    #
-    # for i in range(n):
-    #     for j in range(n):
-    #         if board[i][j] == 1:
-    #             res[i][j] = board[i][j] + 1
 
     cmap = ListedColormap(['white', 'black', 'lightgreen'])
     plt.imshow(board, cmap=cmap, extent=min_max)
     plt.grid(True, color='black', linewidth=1.5)
-    plt.xticks(list(range(0, 9)))
-    plt.yticks(list(range(0, 9)))
+    plt.xticks(list(range(0, n + 1)))
+    plt.yticks(list(range(0, n + 1)))
     plt.title("Chessboard")
     #plt.show()
     plt.savefig('res1')
@@ -62,13 +52,13 @@ def main():
     try:
         var = int(input('Input 1 for a king or 2 for a rook: '))
         assert var in (1, 2)
-        x, y = input('Input coordinates of the chosen figure, format - "1 8"'
-                     ' (2 integer numbers from 1 to 8: ').split()
+        x, y = input(f'Input coordinates of the chosen figure, format - "1 {board_size}"'
+                     f' (2 integer numbers from 1 to {board_size}: ').split()
 
-        assert '1' <= x <= str(board_size) and '1' <= y <= str(board_size)
-        x, y = int(x), int(y)
+        x, y = int(x), int(y)        
+        assert 1 <= x <= board_size and 1 <= y <= board_size
         #y = coord_y[y]
-    except AssertionError:
+    except (ValueError, AssertionError):
         print('Incorrect input!')
     else:
         if var == 1:
